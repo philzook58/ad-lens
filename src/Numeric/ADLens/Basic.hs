871a2ebf39fbba700be1e7b5e7df2f367fb629f8
philzook58 <philzook58@gmail.com>
2018-06-23 18:55:34 -0400

    Added some more basic operations

--- src/Numeric/ADLens/Basic.hs
+++ src/Numeric/ADLens/Basic.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 module Numeric.ADLens.Basic
     ( relu'
     , dup'
@@ -5,7 +6,14 @@ module Numeric.ADLens.Basic
     , sub'
     , div'
     , mul'
+    , sin'
+    , cos'
+    , exp'
     , recip'
+    , fst'
+    , snd'
+    , swap'
+    , assoc'
     ) where
 
 import Numeric.ADLens.Lens
@@ -35,6 +43,31 @@ recip' = lens'' $ \x-> (recip x, \ds -> - ds / (x * x))
 div' :: Fractional a => Lens' (a,a) a 
 div' = lens'' $ (\(x,y) -> (x / y, \d -> (d/y,-x*d/(y * y))))
 
+sin' :: Floating a => Lens' a a
+sin' = lens'' $ \x -> (sin x, \dx -> dx * (cos x))
 
+cos' :: Floating a => Lens' a a
+cos' = lens'' $ \x -> (cos x, \dx -> -dx * (sin x))
 
+pow' :: Num a => Integer -> Lens' a a
+pow' n = lens'' $ \x -> (x ^ n, \dx -> (fromInteger n) * dx * x ^ (n-1)) 
+
+--cmul :: Num a => a -> Lens' a a
+--cmul c = lens (* c) (\x -> \dx -> c * dx)
+
+exp' :: Floating a => Lens' a a
+exp' = lens'' $ \x -> let ex = exp x in
+                      (ex, \dx -> dx * ex)
+
+fst' :: Num b => Lens' (a,b) a
+fst' = lens'' (\(a,b) -> (a, \ds -> (ds, 0)))
+
+snd' :: Num a => Lens' (a,b) b
+snd' = lens'' (\(a,b) -> (b, \ds -> (0, ds)))
+
+swap' :: Lens' (a,b) (b,a)
+swap' = lens'' (\(a,b) -> ((b,a), \(db,da) -> (da, db)))
+
+assoc' :: Lens' ((a,b),c) (a,(b,c))
+assoc' = lens'' $ \((a,b),c) -> ((a,(b,c)), \(da,(db,dc)) -> ((da,db),dc))
 
