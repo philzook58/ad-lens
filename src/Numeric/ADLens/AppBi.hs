e0ec64e8661b894e4b773870c9cc2ff19745e1de
philzook58 <philzook58@gmail.com>
2019-01-27 22:26:43 -0500

    more examples from the bidirecitonal applicative paper

--- src/Numeric/ADLens/AppBi.hs
+++ src/Numeric/ADLens/AppBi.hs
@@ -17,12 +17,12 @@ instance Category Lens where
 
 
 
-
+grad'' (Lens f) x = let (y,j) = (f x) in j 1
 
 lift :: Lens a b -> (forall s. Lens s a -> Lens s b)
 lift l l' = l . l'
 
-unlift :: (forall s. Lens s a -> Lens s b) -> Lens a b
+unlift :: Num a => (forall s. Num s => Lens s a -> Lens s b) -> Lens a b
 unlift f = f id
 
 
@@ -38,12 +38,33 @@ par (Lens f) (Lens g) = Lens l'' where
 fan :: Num s => Lens s a -> Lens s b -> Lens s (a,b)
 fan x y = (par x y) . dup 
 
--- impkredicative polymorphism errror when we use L in type sig
+-- impredicative polymorphism errror when we use L in type sig
 lift2 :: Lens (a,b) c -> (forall s. Num s => (Lens s a, Lens s b) -> Lens s c)
 lift2 l (x,y) = lift l (fan x y)
 
-unlift2 :: (Num a, Num b) => (forall s. (Lens s a, Lens s b) -> Lens s c) -> Lens (a,b) c
+instance (Num a, Num b) => Num (a,b) where
+	(x,y) + (a,b) = (x + a, y + b)
+	(x,y) * (a,b) = (x * a, y * b)
+	abs (x,y) = abs (x,y)
+	fromInteger x = (fromInteger x, fromInteger x)
+
+
+
+-- unlift2 :: (Num a Num b => (Lens (a,b) a, Lens (a,b) b) -> Lens ))
+-- we are unifying s to (a,b)? So we could avoid the funky tuple Num instance.
+-- ALthoug th instance isn't crazy. It makes tuples an elementwise vector
+
+unlift2 :: (Num a, Num b) => (forall s. Num s => (Lens s a, Lens s b) -> Lens s c) -> Lens (a,b) c
 unlift2 f = f (fst', snd')
+ 
+{-
+
+unlift is the BuildInput class
+lift is the fanOutput class
+
+
+-}
+
 
 fst' :: Num b => Lens (a,b) a
 fst' = Lens (\(a,b) -> (a, \ds -> (ds, 0)))
@@ -51,8 +72,8 @@ fst' = Lens (\(a,b) -> (a, \ds -> (ds, 0)))
 snd' :: Num a => Lens (a,b) b
 snd' = Lens (\(a,b) -> (b, \ds -> (0, ds)))
 
-unit :: Lens s () -- ? This isn't right.
-unit = Lens (\s -> ((), const s))
+unit :: Num s => Lens s () -- ? This isn't right.
+unit = Lens (\s -> ((), const 0))
 
 add :: Num a => Lens (a,a) a 
 add = Lens $ \(x,y) -> (x + y, \ds -> (ds, ds))
@@ -69,11 +90,91 @@ recip' = Lens $ \x-> (recip x, \ds -> - ds / (x * x))
 div :: Fractional a => Lens (a,a) a 
 div = Lens $ (\(x,y) -> (x / y, \d -> (d/y,-x*d/(y * y))))
 
+-- They called this "new" Section 3.2
+constLens :: Num s => a -> Lens s a
+constLens x = Lens (const (x, const 0))
+
 -- or rather we might define add = unlift2 (+)
 instance (Num s, Num a) => Num (Lens s a) where
 	x + y = (lift2 add) (x,y)
 	x - y = (lift2 sub) (x,y)
+	x * y = (lift2 mul) (x,y)
 	abs = error "TODO"
+	fromInteger x = constLens (fromInteger x)
+
+t1 :: Num a => Lens (a,a) a
+t1 = unlift2 $ \(x,y) -> x + y*y + y * 7
+
+-- seciton on lifting list functions form biapplicative paper
+-- These are Isos actually.
+lcons :: Lens (a,[a]) [a]
+lcons =  Lens $ \(a,as) -> (a : as, \xs -> (head xs, tail xs))
+lnil :: Lens () [b]
+lnil = Lens $ const ([], const ())
+
+lsequence :: Num s => [Lens s a] -> Lens s [a]
+lsequence [] = lift lnil unit
+lsequence (x : xs) = lift2 lcons (x, lsequence xs)
+
+llift :: Num s => Lens [a] b -> [Lens s a] -> Lens s b
+llift l xs = lift l (lsequence xs)
+
+
+instance (Num a) => Num [a] where
+	(+) = zipWith (+)
+	(*) = zipWith (*)
+	(-) = zipWith (-)
+	abs = map abs
+	fromInteger x = repeat (fromInteger x)
+
+-- We need to hand f a list of the accessor lenses
+-- [Lens [a] a]
+-- This feels quite wrong. Indexing into a list is naughty.
+lunlift :: Num a => (forall s. Num s => [Lens s a] -> Lens s b) -> Lens [a] b
+lunlift f = Lens $ \xs -> 
+					let n = length xs in
+					let inds = [0 .. n-1] in
+					let ls = map (lproj n) inds in
+					let (Lens f') = f ls in
+					f' xs
+
+t2 :: Num a => Lens [a] a					
+t2 = lunlift sum
+t3 :: Num a => Lens [a] a					
+t3 = lunlift product
+
+lproj :: Num a => Int -> Int -> Lens [a] a
+lproj n' ind = Lens $ \xs -> ((xs !! ind), \x' -> replace ind x' zeros) where
+	replace 0 x (y:ys) = x : ys
+	replace n x (y:ys) = y : (replace (n-1) x ys)
+	zeros = replicate n' 0
+
+				
+-- If I auto uncurry everything, that works for me.
+--  The unliftN, doing recursive lift 2.
+{-
+
+class AutoLift a b c where
+	unlift :: (a -> Lens b c)
+
+unlift
+   (a -> b) -> (Lens s a') -> Lens a b'
+
+   lift = lift (Lens)
+   lift (a -> Lens)
+   unlift f = unlift $ \x -> (lift (f x))
+   unlift = unlift . (lift . f)
+
+instance (b ~ (b', b'')  )  => AutoLift (a,a') b c where
+	unlift (
+instance AutoList (Lens a b) where
+	lift :: -> Lens
+AutoUncurry (a -> (b -> c))
+AutoUncurry (a -> Lens) -- may not be able to determine this yet.
+
+Lens a (b -> c) and let's assume b and c are polymorphic enough to keep going?
+
+-}
 
 {-
 -- I need a slot to put a functor. Suspicious. Maybe van Laarhoven representaiton, profunctor? might do something cool here. 
@@ -112,8 +213,8 @@ lensmap (Lens f) = Lens $ \fa ->
 -- Lens a b -> Lens (f a) (f b)
 -- Then we can transform into the other forms via lift/unlift
 
--- ltraverse :: (Applicative f, Applicative t) => Lens a (f b) -> Lens (t a) (f (t b))
--- ltraverse (Lens f) = Lens $ \ta -> let  f 
+
+-- does this make sense? The types work out
 lsequenceA :: (Applicative f, Applicative t, Traversable f, Traversable t) => Lens (t (f a)) (f (t a))
 lsequenceA = Lens $ \tfa -> (sequenceA tfa, sequenceA)
 
@@ -127,7 +228,24 @@ lensfoldl (Lens f) = Lens $ \(s, t) -> let (y, tape) = mapAccumL (curry f) s t  
 lensfoldr :: Traversable t => Lens (a, b) a -> Lens (a, t b) a
 lensfoldr (Lens f) = Lens $ \(s, t) -> let (y, tape) = mapAccumR (curry f) s t  in
 						(y,  \db ->  mapAccumL (\db' f -> (f db')) db tape)						  
+
+t5 = grad'' (lensfoldl mul) (1, [1,1,2,3])
 {-
+-- Not generic enough
+
+instance Ord a => Ord (Lens s a) where
+   x > y = 
+
+
+t6 = unlift $ \x -> if x > 0 then x else 0
+
+-}
+liftC :: Num a => (Lens a b -> Lens c d) -> (forall s. Num s => Lens s a -> Lens s b) -> (forall t. Num t => Lens t c -> Lens t d)
+liftC c f = lift (c (unlift f))
+
+-- t4 = liftC lensfoldl
+
+						{-
 foldr = runState (traverse ) b0 
 foldr' f x0 xs =   < $ fmap f xs :: f (b -> b)
 					   traverse (\x acc -> f x) xs
